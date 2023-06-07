64eb60a3fd747088cc1d0ab95890f7e3c64027f3
fkdlrj125 <39903663+fkdlrj125@users.noreply.github.com>
2023-06-07 17:03:02 +0900

    구조 추가

---
+++ src/test/java/himedia/whatthispills/WhatthispillsApplicationTests.java
@@ -0,0 +1,13 @@
+package himedia.whatthispills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatthispillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
