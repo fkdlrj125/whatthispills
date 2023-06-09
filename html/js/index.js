4f83647accb874d5d469d6e634d189fc237c0611
Ssonggreen <secso6824@gmail.com>
2023-06-09 20:15:13 +0900

    2023.06.09/ HEAD 5~161 간격수정   / / INDEX  5~161 전체 수정 / ADMIN_NUTRI_ADD  5~161 전체 수정 / ADMIN_NUTRI_ADD  5~161 전체 수정 / NUTRI_RESULT  5~161 전체 수정 / FIND_PWD  5~161 전체 수정 / USER_LIKES  5~161 전체 수정 및 찜 기능 추가

--- html/js/index.js
+++ html/js/index.js
@@ -1,9 +1,9 @@
-let mainText = document.querySelector("#sle1");
-window.addEventListener("scroll", function() {
+const mainText = document.querySelector("#sle1");
+window.addEventListener("scroll", function () {
     let value = this.window.scrollY;
     console.log("scrollY", value);
 
-    if (value > 200){
+    if (value > 200) {
         mainText.style.animation = 'slides 1s ease-out forwards';
     } else {
         mainText.style.animation = "outslide 1s ease-out forwards";
