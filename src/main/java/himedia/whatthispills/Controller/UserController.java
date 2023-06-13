587f791ff88b523d59445bc628fad8efc7992ad0
fkdlrj125 <39903663+fkdlrj125@users.noreply.github.com>
2023-06-13 15:36:22 +0900

    230613-UserController.java, UserService.java, UserRepository.java, JDBCUserRepository.java, join.css-컨트롤러, 서비스, 레파지토리 추가 / join.css 수정

--- src/main/java/himedia/whatthispills/Controller/UserController.java
+++ src/main/java/himedia/whatthispills/Controller/UserController.java
@@ -1,5 +1,120 @@
 package himedia.whatthispills.Controller;
 
-public class UserController {
+import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import himedia.whatthispills.Domain.User;
+import himedia.whatthispills.Domain.UserLikes;
+import himedia.whatthispills.Service.UserService;
+
+@Controller
+public class UserController {
+	private final UserService userService;
+	
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@GetMapping
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/join")
+	public String join_get() {
+		return "join";
+	}
+	@PostMapping("/join")
+	public String join_post(@ModelAttribute User addUser) {
+		userService.saveUser(addUser);
+		return "redirect:/login";
+	}
+	@GetMapping("/login")
+	public String login_get() {
+		
+		return "login";
+	}
+	@PostMapping("/login")
+	public String login_post(@RequestParam String user_email, @RequestParam String user_pwd, HttpServletRequest request) {
+		if(userService.login(user_email, user_pwd)) {
+			HttpSession session = request.getSession();
+			session.setAttribute("passUser", userService.findEmail(user_email));
+		}
+		return "redirect:/";
+	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpServletRequest request) {
+		request.getSession().invalidate();
+		return ""; // 현재 페이지로 이동
+	}
+	
+	@GetMapping("/user/find_pwd")
+	public String find_pwd_get() {
+		return "find_pwd";
+	}
+	@PostMapping("/user/find_pwd")
+	public String find_pwd_post(String user_email, String user_name, Model model, HttpServletRequest request) {
+		if(userService.find_pwd(user_email, user_name)) {
+			request.getSession().setAttribute("user_email", user_email);
+			return "redirect:/user/reset_pwd";
+		}
+		model.addAttribute(""); //alert 문구
+		return "find_pwd";
+	}
+	@GetMapping("/user/reset_pwd")
+	public String reset_pwd_get(String user_email) {
+		return "reset_pwd";
+	}
+	@PostMapping("/user/reset_pwd")
+	public String reset_pwd_post(@SessionAttribute String user_email, String user_pwd, HttpServletRequest request, Model model) {
+		if(userService.updatePassword(user_email, user_pwd)) {
+			request.getSession().invalidate();
+			return "redirect:/login";
+		}
+		model.addAttribute(""); // alert 문구
+		return "redirect:/user/reset_pwd";
+	}
+	
+	@GetMapping("/user/edit")
+	public String user_edit_get(@SessionAttribute User passUser, Model model) {
+		model.addAttribute("user", passUser);
+		return "user/edit";
+	}
+	
+	@PostMapping("/user/edit")
+	public String user_edit_post(@ModelAttribute User updateUser, HttpServletRequest request) {
+		User user = userService.update(updateUser.getIdx(), updateUser);
+		request.getSession().setAttribute("passUser", user);
+		return "redirect:/user/info";
+	}
+	
+	@GetMapping("/user/info")
+	public String user_info(@SessionAttribute User passUser, Model model) {
+		model.addAttribute("user", passUser);
+		return "user/info";
+	}
+	
+	@GetMapping("/user/likes")
+	public String user_likes(@SessionAttribute User passUser, Model model) {
+		List<UserLikes> like_list = userService.userLikeList(passUser.getIdx());
+		model.addAttribute("like_list", like_list);
+		return "user/likes";
+	}
+	
+	@GetMapping("/user/menu")
+	public String user_menu() {
+		return "user/menu";
+	}
+	
 }
--- src/main/java/himedia/whatthispills/Repository/JDBCUserRepository.java
+++ src/main/java/himedia/whatthispills/Repository/JDBCUserRepository.java
@@ -1,5 +1,101 @@
 package himedia.whatthispills.Repository;
 
-public class JDBCUserRepository {
+import java.sql.ResultSet;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import himedia.whatthispills.Domain.User;
+import himedia.whatthispills.Domain.UserLikes;
+
+@Repository
+public class JDBCUserRepository implements UserRepository{
+	private final JdbcTemplate jdbcTemplate;
+	
+	public JDBCUserRepository(DataSource dataSource) {
+		jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
+	public RowMapper<User> userMapper() {
+		return (ResultSet rs, int rowNum) -> {
+			User user = new User(
+					rs.getString("user_email"),
+					rs.getString("user_name"),
+					rs.getString("user_pwd"),
+					rs.getString("user_birth"),
+					rs.getString("user_gender"));
+			
+			return user;
+		};
+	}
+	
+	public RowMapper<UserLikes> userLikesMapper() {
+		return (ResultSet rs, int rowNum) -> {
+			UserLikes userlikes = new UserLikes(
+					rs.getLong("user_idx"),
+					rs.getLong("nutri_idx"),
+					rs.getBoolean("likes"));
+			
+			return userlikes;
+		};
+	}
+
+	@Override
+	public User save(User user) {
+		SimpleJdbcInsert insert = new SimpleJdbcInsert(jdbcTemplate).withTableName("user_").usingGeneratedKeyColumns("user_idx");
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("user_email", user.getEmail());
+		map.put("user_name", user.getName());
+		map.put("user_pwd", user.getPwd());
+		map.put("user_age", user.getBirth());
+		map.put("user_gender", user.getGender());
+		Number result = insert.executeAndReturnKey(new MapSqlParameterSource(map));
+		user.setIdx(result.longValue());
+		return null;
+	}
+
+	@Override
+	public Optional<User> findByEmail(String email) {
+		List<User> userList = jdbcTemplate.query("select * from user_ where user_email like ?", userMapper(), email);
+		return userList.stream().findAny();
+	}
+
+	@Override
+	public Optional<User> findByEmailName(String email, String name) {
+		List<User> userList = jdbcTemplate.query("select * from user_ where user_email like ? and user_name like ?", userMapper(), name, email);
+		return userList.stream().findAny();
+	}
+
+	@Override
+	public List<User> findAll() {
+		return jdbcTemplate.query("select * from user_", userMapper());
+	}
+	
+	@Override
+	public List<UserLikes> userLikes(Long user_idx) {
+		return jdbcTemplate.query("select * from nutri_like where user_idx = ?", userLikesMapper(), user_idx);
+	}
+
+	@Override
+	public Optional<User> updateUser(Long update_idx, User update_user) {
+		jdbcTemplate.update("update user_ set user_pwd = ?, user_gender = ?,user_birth = ? where user_idx = ?", 
+				update_user.getPwd(), update_user.getGender(), update_user.getBirth(), update_idx);
+		return findByEmail(update_user.getEmail());
+	}
+
+	@Override
+	public Optional<User> updatePwd(String user_email, String update_pwd) {
+		jdbcTemplate.update("update user_ set user_pwd = ? where user_email = ?", 
+				update_pwd, user_email);
+		return findByEmail(user_email);
+	}
 }
