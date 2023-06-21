b152723063af02606cde43e4da6c3c918e625cf4
Ssonggreen <secso6824@gmail.com>
2023-06-21 18:46:45 +0900

    Revert "23.06.21/nutriRepository&nutriService test완료/"
    This reverts commit bd128016379cc46f6874e975442dac0c05298dcb.

--- src/test/java/himedia/whatthispills/Service/NutriServiceTest.java
+++ src/test/java/himedia/whatthispills/Service/NutriServiceTest.java
@@ -1,87 +1,17 @@
 package himedia.whatthispills.Service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import java.util.List;
 import java.util.Optional;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Service;
 
 import himedia.whatthispills.Domain.Nutri;
-import himedia.whatthispills.Repository.JDBCNutriRepository;
+import himedia.whatthispills.Repository.NutriRepository;
 
 @SpringBootTest
-@Transactional
 public class NutriServiceTest {
-	@Autowired
-	NutriService service;
-	@Autowired
-	JDBCNutriRepository repository;
-	
-	
-	@Test
-	void 이름검색() {
-		Nutri nutri = new Nutri(
-				1235L,
-				"영양제1",
-				"영양제2",
-				"영양제3",
-				"영양제4",
-				"영양제5",
-				"영양제6",
-				"영양제7",
-				"영양제8",
-				"영양제9",
-				"영양제11"
-				);
-		repository.save(nutri); //give
-		List<Nutri> result = service.findByNameNutri("영양제"); //when
-		assertThat(result.equals(nutri)); //
-	}
-	
-	@Test
-	void 상세페이지() {
-		Nutri nutri = new Nutri(
-				1235L,
-				"영양제1",
-				"영양제2",
-				"영양제3",
-				"영양제4",
-				"영양제5",
-				"영양제6",
-				"영양제7",
-				"영양제8",
-				"영양제9",
-				"영양제11"
-				);
-		repository.save(nutri);
-		Optional<Nutri> result = service.findByNameInfo("영양제1");
-		assertThat(result.equals(nutri));
-	}
-	@Test
-	void 카테고리이동() {
-		Nutri nutri = new Nutri(
-				1235L,
-				"영양제1",
-				"영양제2",
-				"영양제3",
-				"영양제4",
-				"영양제5",
-				"영양제6",
-				"영양제7",
-				"영양제8",
-				"영양제9",
-				"영양제11"
-				);
-		repository.save(nutri);
-		List<Nutri> result = service.findByCategory("영양");
-		assertThat(result.equals(nutri));
-	}
-	
-}
-	
 	
 
+
+}
